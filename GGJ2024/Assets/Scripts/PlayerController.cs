5615bdfb22a79406fa19deeae11edc6ec1fd4bbd
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-01-26 09:59:37 +0200

    added tomer attack

--- GGJ2024/Assets/Scripts/PlayerController.cs
+++ GGJ2024/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,32 @@ using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] CharacterController CharController;
+    [SerializeField] PlayerFist playerFist;
 
-   
     public float speed = 4;
     public Vector3 jumpHeight = new Vector3(0, 2.5f, 0);
     public Vector3 gravity = new Vector3(0, -2.5f, 0);
+  
+    // attack varible
+    public float punchTime;
+    public float attackInterval;
+    public float comboMultiplier;
+    public float comboWindow;
+    public float invunrabiltyInterval;
+    private float _comboCounter = 0;
+    private KeyCode _attackKey = KeyCode.E;
+    private bool _isInCombo = false;
+    private bool _canAttack = true;
+    private bool _isInvunrable = false;
+
+    private MyTimer _comboT;
+    private MyTimer _attackT;
+    private MyTimer _invurnebltyT;
 
     private float _laughMeter; // laughMeter
     private float _damage;
     private float _knockBack;
-    private float _comboReset;
+
     private float _stunDur;
     private bool _isGrounded = true;
     private bool _isJumping = false;
@@ -29,6 +45,12 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 originalPosition;
 
+    private void Start()
+    {
+        _comboT = new MyTimer(comboWindow);
+        _attackT = new MyTimer(attackInterval);
+        _invurnebltyT = new MyTimer(invunrabiltyInterval);
+    }
     void Update()
     {
         if (_isGrounded)
@@ -53,7 +75,58 @@ public class PlayerController : MonoBehaviour
                 _isJumping = false;
             }
         }
+        { // Attack interval timing
 
+            //Waiting for the attack interval to end
+            if (_attackT.IsOver())
+            {
+                _attackT.Reset();
+                _canAttack = true;
+            }
+            if (_canAttack)
+            {
+                // Checking for attack input
+                if (Input.GetKeyDown(_attackKey))
+                {
+                    if (_isInCombo)
+                    {
+                        _attackT.Reset();
+                    }
+                    Attack();
+                }
+            }
+            else
+            {
+                _attackT.Tick();
+            }
+        }
+
+        { // Combo timing
+            if (_isInCombo && _canAttack)
+            {
+                _comboT.Tick();
+                //Missed the window to chain combo
+                if (_comboT.IsOver())
+                {
+                    _comboT.Reset();
+                    Debug.Log("at timer");
+                    ComboEnd();
+                }
+            }
+        }
+
+        { // Invulnerability timing
+            if (_isInvunrable)
+            {
+                _invurnebltyT.Tick();
+
+                if (_invurnebltyT.IsOver())
+                {
+                    _invurnebltyT.Reset();
+                    _isInvunrable = false;
+                }
+            }
+        }
 
     }
     public void Move()
@@ -84,19 +157,56 @@ public class PlayerController : MonoBehaviour
 
     public void Attack() 
     {
-        // Creates an attack that hits enemies infront of the Character
+        _canAttack = false;
+        switch (_comboCounter)
+        {
+            case 0:
+                playerFist.Attack(_damage, 0);
+                Debug.Log("First hit");
+                break;
+            case 1:
+                playerFist.Attack(_damage * comboMultiplier, 0);
+                Debug.Log("Second hit");
+                break;
+            case 2:
+                playerFist.Attack(_damage * comboMultiplier * comboMultiplier, _knockBack);
+                Debug.Log("Third hit");
+                break;
+            default:
+                Debug.Log("I fucked up");
+                break;
+        }
     }
 
     public void TakeDamage()
     {
-        // if a character has been attacked by an enemy should invoke TakeDamage in the player script (called by enemey script Attack())
+        if (!_isInvunrable)
+        {
+            //The rest of the code
+            _isInvunrable = true;
+        }
     }
 
     public void IncreaseCombo()
     {
-        // increases the combo depending on if combo didn't reset and and the player hit a target 
+        {
+            // increases the combo depending on if combo didn't reset and and the player hit a target 
+            Debug.Log("Combo Increased" + _comboCounter);
+            _isInCombo = true;
+            _comboCounter++;
+            if (_comboCounter > 2)
+            {
+                Debug.Log("at increase");
+                ComboEnd();
+            }
+        }
+
     }
 
-    
-
+    public void ComboEnd()
+    {
+        Debug.Log("Combo Over");
+        _comboCounter = 0;
+        _isInCombo = false;
+    }
 }
