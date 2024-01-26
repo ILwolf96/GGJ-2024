359bb50b86a97f9806190d14a0288a26f18313e4
RoxasCodes <evicokazzz@gmail.com>
2024-01-26 15:02:42 +0200

    first push

---
+++ GGJ2024/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    public GameObject player;
+
+
+    public void DamageBoost()
+    {
+        //player.BoostPlayerDamage()
+        print("damage boost");
+    }
+
+    public void SpeedBoost()
+    {
+        //player.BoostPlayerSpeed()
+        print("speed boost");
+    }
+
+    public void CritChance()
+    {
+        //player.BoostPlayerCrit()
+        print("crit chance boost");
+    }
+
+}
