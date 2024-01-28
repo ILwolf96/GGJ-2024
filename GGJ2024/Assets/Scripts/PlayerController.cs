9ed0428b1022a7a477ff27169611d84841b39f65
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-01-28 15:31:10 +0200

    Create Build

--- GGJ2024/Assets/Scripts/PlayerController.cs
+++ GGJ2024/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
